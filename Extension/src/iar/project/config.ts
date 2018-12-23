bb5b64814f0b64b04c30e169a56921c19f026c9b
Philip Luyckx <philip.luyckx@gmail.com>
2018-12-23 12:36:37 +0100

    Expand config interface with a name field

--- Extension/src/iar/project/config.ts
+++ Extension/src/iar/project/config.ts
@@ -6,26 +6,42 @@ import { XmlNode } from '../../utils/XmlNode';
 import { Define } from './define';
 import { IncludePath } from './includepaths';
 import { PreIncludePath } from './preincludepath';
+import { IarXml } from "../../utils/xml";
 
 export interface Config {
+    readonly name: string;
     readonly defines: Define[];
     readonly includes: IncludePath[];
     readonly preIncludes: PreIncludePath[];
 }
 
 class XmlConfig implements Config {
+    private xml: XmlNode;
+
     readonly defines: Define[];
     readonly includes: IncludePath[];
     readonly preIncludes: PreIncludePath[];
 
     constructor(xmlConfigElement: XmlNode, ewpPath: Fs.PathLike) {
-        if (xmlConfigElement.tagName !== 'configuration') {
-            throw new Error("Expected an xml element 'configuration' instead of '" + xmlConfigElement.tagName + "'");
+        this.xml = xmlConfigElement;
+
+        if (this.xml.tagName !== 'configuration') {
+            throw new Error("Expected an xml element 'configuration' instead of '" + this.xml.tagName + "'");
         }
 
         this.defines = Define.fromXml(xmlConfigElement);
         this.includes = IncludePath.fromXmlData(xmlConfigElement, ewpPath);
         this.preIncludes = PreIncludePath.fromXml(xmlConfigElement, ewpPath);
+    }
+
+    get name(): string {
+        let name = IarXml.getNameTextFromElement(this.xml);
+
+        if (name === undefined) {
+            return "";
+        } else {
+            return name;
+        }
     }
 }
 
