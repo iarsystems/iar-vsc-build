989ecd27bd26550f2af69c87fd83eed61fbd51c9
Philip Luyckx <philip.luyckx@gmail.com>
2018-12-28 17:11:49 +0100

    Create a first small implementation to test UI

--- Extension/src/main.ts
+++ Extension/src/main.ts
@@ -2,10 +2,23 @@
 
 
 import * as vscode from 'vscode';
+import { UI } from './extension/ui/app';
+import { ToolManager } from './iar/tools/manager';
+import { Settings } from './extension/settings';
 
 export function activate(context: vscode.ExtensionContext) {
+    UI.createUi(IarVsc.toolManager, context);
 
+    let roots = Settings.getIarInstallDirectories();
+
+    roots.forEach(path => {
+        IarVsc.toolManager.collectFrom(path);
+    });
 }
 
 export function deactivate() {
+}
+
+namespace IarVsc {
+    export let toolManager = ToolManager.createIarToolManager();
 }
