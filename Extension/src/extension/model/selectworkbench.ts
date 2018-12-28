6aed9fa378f38132a5cf94fb958b4f484183574b
Philip Luyckx <philip.luyckx@gmail.com>
2018-12-28 17:09:56 +0100

    Add a model for the select workbench UI

---
+++ Extension/src/extension/model/selectworkbench.ts
@@ -0,0 +1,63 @@
+
+'use strict';
+
+import { ListInputModelBase } from "./model";
+import { Workbench } from "../../iar/tools/workbench";
+
+export class WorkbenchListModel extends ListInputModelBase<Workbench> {
+    private workbenches: Workbench[];
+
+    selected: Workbench | undefined;
+
+    constructor(...workbenches: Workbench[]) {
+        super();
+
+        this.workbenches = workbenches;
+    }
+
+    public setWorkbenches(...workbenches: Workbench[]) {
+        this.workbenches = workbenches;
+
+        this.fireInvalidateEvent();
+    }
+
+    get amount(): number {
+        return this.workbenches.length;
+    }
+
+    get selectedText(): string {
+        if (this.selected) {
+            return this.selected.name;
+        } else {
+            return "";
+        }
+    }
+
+    label(index: number): string {
+        return this.workbenches[index].name;
+    }
+    description(index: number): string | undefined {
+        return this.workbenches[index].path.toString();
+    }
+    detail(index: number): string | undefined {
+        return this.workbenches[index].idePath.toString();
+    }
+
+    select(index: number): boolean {
+        let tmp = this.workbenches[index];
+
+        if (!this.selected) {
+            this.selected = tmp;
+            this.fireSelectionChanged(this.selected);
+
+            return true;
+        } else if (this.selected.idePath !== tmp.idePath) {
+            this.selected = tmp;
+            this.fireSelectionChanged(this.selected);
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
