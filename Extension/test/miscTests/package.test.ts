8145c5b0e8a01247aaededbf63c14178dee6eeb0
Hampus Adolfsson <hampus.adolfsson@iar.com>
2021-10-18 16:02:14 +0200

    Test to ensure all commands are activation events

---
+++ Extension/test/miscTests/package.test.ts
@@ -0,0 +1,31 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+'use strict';
+
+import * as path from "path";
+import * as Assert from "assert";
+
+// Here, we can put sanity checks on the package.json metadata
+suite("Test package.json", () => {
+    test("All commands are activation events", () => {
+        // When a command is run, we need to tell vs code to activate our extension so that we can handle it
+        // If the extension is not activated, the user will receive a 'command not found' error
+        // Technically, there are some commands that will only ever be called when the extension is already activated,
+        // but it's probably safer to add them all anyway
+
+        const packageJsonPath = path.join(__dirname, "../../../package.json");
+        const packageJson = require(packageJsonPath);
+
+        const activationEvents: string[] = packageJson["activationEvents"];
+
+        const commands = packageJson["contributes"]["commands"];
+        const commandNames: string[] = commands.map((command: any) => command["command"]);
+
+        commandNames.forEach(commandName => {
+            Assert(activationEvents.some(event => event === `onCommand:${commandName}`),
+                    `Command '${commandName} is not in the activation events'`);
+        });
+    });
+});
--- Extension/test/runTests.ts
+++ Extension/test/runTests.ts
@@ -6,6 +6,7 @@ async function main() {
     await runTestsIn(path.resolve(__dirname),'../../', './unitTests/index');
     await runTestsIn(path.resolve(__dirname),'../../','./integrationTests/index');
     await runTestsIn(path.resolve(__dirname),'../../','./vscodeTests/index', '../../test/vscodeTests/TestProjects' );
+    await runTestsIn(path.resolve(__dirname),'../../','./miscTests/index');
 }
 
 main();
