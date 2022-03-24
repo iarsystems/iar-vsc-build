5baf5d064bad1b965c1c80deac95f159de38e1d6
Hampus Adolfsson <hampus.adolfsson@iar.com>
2022-03-24 11:13:10 +0100

    VSC-237 Restructure EW version checks

--- Extension/src/iar/tools/workbenchVersionRegistry.ts
+++ Extension/src/iar/tools/workbenchVersionRegistry.ts
@@ -1,38 +1,40 @@
+/* eslint-disable comma-spacing */
 import { Workbench } from "./workbench";
 
 /**
  * Allows looking up information about a workbench based on its IDE platform version.
- * This can be used e.g. to handle API changes between versions or handle bugs in specific versions.
+ * This can be used e.g. to handle API changes between versions or to handle bugs in specific versions.
  */
-export namespace WorkbenchVersionRegistry {
-    // TODO: once we start adding more checks here, find a clearer way to express them.
-    /**
-     * Whether this workbench can return correct C-SPY command lines from the project
-     * manager service. Previous versions may return incorrect command lines, see MAJ-156.
-     */
-    export function canFetchCSpyCommandLine(workbench: Workbench): boolean {
-        const version = workbench.version;
-        // Fix versions: 9.0.12 and 9.1.1
-        return version.major > 9 ||
-            (version.major === 9 && (
-                (version.minor > 1 ||
-                    (version.minor === 1 && version.patch > 0) ||
-                    (version.minor === 0 && version.patch > 11)
-                )));
-    }
+export namespace WorkbenchVersions {
+    type FixVersion = [number, number, number];
 
     /**
+     * We make no attempt to support versions below this; some things may still work... */
+    export const supportsVSCode: FixVersion = [8,0,0];
+    /**
+     * Whether this workbench can return correct C-SPY command lines from the project
+     * manager service. Previous versions may return incorrect command lines, see MAJ-156. */
+    export const canFetchCSpyCommandLine: FixVersion = [9,1,1];
+    /**
      * Whether this workbench supports the SetNodeByIndex thrift procedure, which
-     * te
      * fixes issues with the previous SetNode procedure. See VSC-233.
      */
-    export function supportsSetNodeByIndex(workbench: Workbench): boolean {
-        const version = workbench.version;
-        // Fix version: 9.1.1
-        return version.major > 9 ||
-            (version.major === 9 && (
-                version.minor > 1 ||
-                    (version.minor === 1 && version.patch > 0)
-            ));
+    export const supportsSetNodeByIndex: FixVersion = [9,1,1];
+
+    /**
+     * Checks whether a workbench version meets the given fix version.
+     */
+    export function doCheck(workbench: Workbench, fixVer: FixVersion) {
+        const ewVersion = [workbench.version.major, workbench.version.minor, workbench.version.patch];
+        for (let i = 0; i < 3; i++) {
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            if (ewVersion[i]! > fixVer[i]!) {
+                return true;
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            } else if (ewVersion[i]! < fixVer[i]!) {
+                return false;
+            }
+        }
+        return true;
     }
 }
