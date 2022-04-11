f22ba5aa527558b22428fb73b9ba9e6e567f6e79
Hampus Adolfsson <hampus.adolfsson@iar.com>
2022-04-11 15:09:32 +0200

    Deactivate extension after running tests

--- Extension/test/vscodeTests/index.ts
+++ Extension/test/vscodeTests/index.ts
@@ -3,9 +3,14 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 import * as path from "path";
+import { deactivate } from "../../src/extension/main";
 import * as utils from "../../utils/testutils/testUtils";
 
-export function run(): Promise<void> {
+export async function run(): Promise<void> {
     const testsRoot = path.resolve(__dirname);
-    return utils.getTestPromise(testsRoot, 20000);
+    await utils.getTestPromise(testsRoot, 20000);
+    // Since these tests activate the extension, we should deactivate it afterwards to ensure
+    // e.g. the service launcher is disposed of. Otherwise, the test process hangs under some circumstances.
+    // TODO: investigate whether deactivate is supported to be called by vs code here.
+    deactivate();
 }
