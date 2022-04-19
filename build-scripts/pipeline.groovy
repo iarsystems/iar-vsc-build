a20ffc42c3bdb0a4b7b57ffac4841e08510bd49d
Hampus Adolfsson <hampus.adolfsson@iar.com>
2022-04-19 14:42:07 +0200

    VSC-242 Add multibranch pipeline script

---
+++ build-scripts/pipeline.groovy
@@ -0,0 +1,13 @@
+@Library('common@tags/1.3.0') _
+
+node {
+	stage('Load pipeline') {
+		deleteDir()
+		dir('release-scripts') {
+			git credentialsId: 'gitsshkey', url: 'ssh://git@gitlab.iar.se:2222/ide/vscode/releng/release-scripts.git', branch: "master"
+			runner = load "release/iar-vsc-build.groovy"
+		}
+	}
+}
+
+runner.build_and_test("$BRANCH_NAME")
