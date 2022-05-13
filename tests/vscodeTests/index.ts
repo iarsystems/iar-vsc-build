d6c1c0abcc1ac0f3727f858670a4069495a28bac
Hampus Adolfsson <hampus.adolfsson@iar.com>
2022-05-13 10:43:52 +0200

    Reenable manual deactivation after vscode test suite
    This was supposed to be fixed upstream, but their change
    doesn't seem to have fixed the issue.

--- tests/vscodeTests/index.ts
+++ tests/vscodeTests/index.ts
@@ -3,9 +3,17 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 import * as path from "path";
+import { deactivate } from "../../src/extension/main";
 import * as utils from "iar-vsc-common/testutils/testUtils";
 
 export function run(): Promise<void> {
     const testsRoot = path.resolve(__dirname);
-    return utils.getTestPromise(testsRoot, 20000);
+    return utils.getTestPromise(testsRoot, 20000).finally(async() => {
+        console.log("Tests done, deactivating");
+        // Since these tests activate the extension, we should deactivate it afterwards to ensure
+        // e.g. the service launcher is disposed of. Otherwise, the test process hangs under some circumstances.
+        // TODO: investigate whether deactivate is supposed to be called by vs code here.
+        await deactivate();
+        console.log("Done with deactivation");
+    });
 }
