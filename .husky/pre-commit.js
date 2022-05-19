b5fb9a490a595f1bb96d2902b3cb01c1eb0f9c26
Hampus Adolfsson <hampus.adolfsson@iar.com>
2022-05-19 14:53:41 +0200

    VSC-255 Add commit hook for license header

--- .husky/pre-commit.js
+++ .husky/pre-commit.js
@@ -2,6 +2,8 @@
 // This hook forces the source for the "iar-vsc-common" dependency to point to the public repository on IAR System's github.
 // During development it's practical to use other sources for this dependency (i.e. a local clone); this hook should
 // prevent accidental commits of such changes.
+//
+// This hook also forces all source files to have the MPLv2 license header at the start of the file.
 /// -----------------------------
 
 const { spawnSync } = require("child_process");
@@ -14,6 +16,10 @@ function runAndGetStdout(command, callback){
         callback(stdout);
     });
 };
+
+const MPLv2_header = `/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */`
 
 runAndGetStdout("git diff --cached --name-only --diff-filter=ACMR", function(changedFiles) {
     const regex = "git@github\\.com:IARSystems/iar-vsc-common\\.git#[\\da-fA-F]{8}"
@@ -39,5 +45,19 @@ runAndGetStdout("git diff --cached --name-only --diff-filter=ACMR", function(cha
                 exit(1);
             }
         });
+    }
+
+    const filesArray = changedFiles.trim().split("\n");
+    // Check that all typescript files have the mpl preamble
+    const res = filesArray.filter(file => file.endsWith(".ts")).map(file => {
+        const contents = readFileSync(file).toString().replace(/\r\n/g, "\n");
+        if (!contents.startsWith(MPLv2_header)) {
+            console.log("Missing or malformed MPLv2 license header in file: " + file);
+            return false;
+        }
+        return true;
+    });
+    if (!res.every(v => v)) {
+        exit(1);
     }
 });
