3fd36c8761cbf9e8837ffe86b9b4fd9060b7c0e8
Hampus Adolfsson <hampus.adolfsson@iar.com>
2022-05-19 13:42:16 +0200

    VSC-255 Add license preamble to source files

--- src/extension/api.ts
+++ src/extension/api.ts
@@ -1,3 +1,6 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { BuildExtensionApi } from "iar-vsc-common/buildExtension";
 import { OsUtils } from "iar-vsc-common/osUtils";
 import { ExtensionState } from "./extensionstate";
--- src/iar/tools/workbenchversionregistry.ts
+++ src/iar/tools/workbenchversionregistry.ts
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 /* eslint-disable comma-spacing */
 import { Workbench, WorkbenchType } from "iar-vsc-common/workbench";
 
