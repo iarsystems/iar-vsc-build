2321ec0debcb4c93a982fd136c98a24a7f0f18bc
Hampus Adolfsson <hampus.adolfsson@iar.com>
2022-09-09 15:56:32 +0200

    VSC-173 Fix source config tests for multitarget

--- tests/vscodeTests/projectview.test.ts
+++ tests/vscodeTests/projectview.test.ts
@@ -36,10 +36,12 @@ suite("Test Project View", ()=>{
         this.timeout(50000);
         await VscodeTestsUtils.ensureExtensionIsActivated();
         const sandboxPath = VscodeTestsSetup.setup();
-        projectDir = Path.join(sandboxPath, "SourceConfiguration/IAR-STM32F429II-EXP/LedFlasher");
-        libSrcDir = Path.join(sandboxPath, "SourceConfiguration/STM32F4xx_DSP_StdPeriph_Lib/Libraries/STM32F4xx_StdPeriph_Driver/src");
+        projectDir = Path.join(sandboxPath, "SourceConfiguration/Project");
+        libSrcDir = Path.join(sandboxPath, "SourceConfiguration/Library/src");
 
-        await VscodeTestsUtils.activateProject("LedFlasher");
+        await VscodeTestsUtils.activateProject("SourceConfiguration");
+        // Give the files view time to refresh
+        await new Promise((res, _) => setTimeout(res, 1000));
     });
 
     setup(function() {
@@ -48,7 +50,7 @@ suite("Test Project View", ()=>{
 
     test("Displays project files", async() => {
         const rootFileNodes = await Utils.getNodes();
-        Assert.strictEqual(rootFileNodes.length, 5);
+        Assert.strictEqual(rootFileNodes.length, 3);
         const rootFileItems = await Utils.toTreeItems(rootFileNodes);
         const mainItem = rootFileItems.find(item => item.label === "main.c");
         Assert(mainItem);
@@ -56,16 +58,16 @@ suite("Test Project View", ()=>{
         Assert(OsUtils.pathsEqual(mainItem.resourceUri.fsPath, Path.join(projectDir, "main.c")));
         Assert.strictEqual(mainItem.collapsibleState, Vscode.TreeItemCollapsibleState.None);
 
-        const subgroupIndex = rootFileItems.findIndex(item => item.label === "STM32F4xx_StdPeriph_Driver");
+        const subgroupIndex = rootFileItems.findIndex(item => item.label === "lib");
         Assert.strictEqual(rootFileItems[subgroupIndex]?.contextValue, "group");
         Assert.strictEqual(rootFileItems[subgroupIndex]?.collapsibleState, Vscode.TreeItemCollapsibleState.Expanded);
         const subgroupNodes = await Utils.getNodes(rootFileNodes[subgroupIndex]);
-        Assert.strictEqual(subgroupNodes.length, 6);
+        Assert.strictEqual(subgroupNodes.length, 1);
         const subgroupItems = await Utils.toTreeItems(subgroupNodes);
-        const gpioItem = subgroupItems.find(item => item.label === "stm32f4xx_gpio.c");
+        const gpioItem = subgroupItems.find(item => item.label === "gpio.c");
         Assert(gpioItem);
         Assert(gpioItem.resourceUri);
-        Assert(OsUtils.pathsEqual(gpioItem.resourceUri.fsPath, Path.join(libSrcDir, "stm32f4xx_gpio.c")));
+        Assert(OsUtils.pathsEqual(gpioItem.resourceUri.fsPath, Path.join(libSrcDir, "gpio.c")));
         Assert.strictEqual(gpioItem.collapsibleState, Vscode.TreeItemCollapsibleState.None);
     });
 });
